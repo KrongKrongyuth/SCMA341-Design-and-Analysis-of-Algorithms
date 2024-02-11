c5ba85d2071b7f7889cf92adf3fc6057a56db779
TON_KKrongyuth <ton.kkrongyuth@gmail.com>
2024-02-11 17:30:51 +0700

    commit templete

--- Competition/competition_1_logestsubstring.py
+++ Competition/competition_1_logestsubstring.py
@@ -1,3 +1,34 @@
 """
 This file for longest substring search algorithm
 """
+
+class Competition():
+    def __init__(self, text_1, text_2):
+        self.text_1 = text_1
+        self.text_2 = text_2
+
+    def tao_algorithm(self):
+        """
+        Algorithm from tao
+        """
+
+    def kantong_algorithm(self):
+        """
+        Algorithm from tao
+        """
+
+    def tonkaow_algorithm(self):
+        """
+        Algorithm from tao
+        """
+
+    def ton_algorithm(self):
+        """
+        Algorithm from tao
+        """
+
+if __name__ == "__main__":
+    text_1 = "parachute"
+    text_2 = "shuttle"
+    com = Competition(text_1 = text_1, text_2 = text_2)
+    com.ton_algorithm()     # How to run your function.
