289093fd33e640231e986bfb48fc320576e38830
TON_KKrongyuth <ton.kkrongyuth@gmail.com>
2024-02-12 11:22:17 +0700

    Ton commit Logest substring search (using numpy)

--- Competition/competition_1_logestsubstring.py
+++ Competition/competition_1_logestsubstring.py
@@ -14,18 +14,32 @@ class Competition():
 
     def kantong_algorithm(self):
         """
-        Algorithm from tao
+        Algorithm from kantong
         """
 
     def tonkaow_algorithm(self):
         """
-        Algorithm from tao
+        Algorithm from tonkaow
         """
 
     def ton_algorithm(self):
         """
-        Algorithm from tao
+        Algorithm from ton
         """
+        import numpy as np
+        possible_result, i = [], 0
+        while i < len(self.text_1):
+            j, k, word = 0, 0, ""
+            while j + k < len(self.text_2):
+                if (text_1[i+k] == text_2[j+k]):
+                    word += text_2[j+k]
+                    k += 1
+                j += 1
+            if k != 0: possible_result.append([word, k])
+            i += 1
+        result = possible_result[np.argmax(np.array(possible_result)[:,1])]
+        print(f"\nLongest substring is \"{result[0]}\" with \"{result[1]}\" letters.\n")
+        return result
 
 if __name__ == "__main__":
     text_1 = "parachute"
