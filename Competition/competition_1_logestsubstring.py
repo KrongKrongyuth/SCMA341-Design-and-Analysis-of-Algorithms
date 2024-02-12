174e6c89f29ddd6192bbb6df0c5a567f98f85239
TON_KKrongyuth <ton.kkrongyuth@gmail.com>
2024-02-12 19:28:57 +0700

    Ton commit add comment

--- Competition/competition_1_logestsubstring.py
+++ Competition/competition_1_logestsubstring.py
@@ -27,19 +27,19 @@ class Competition():
         Algorithm from ton
         """
         import numpy as np
-        possible_result, i = [], 0
-        while i < len(self.text_1):
-            j, k, word = 0, 0, ""
-            while j < len(self.text_2):
-                if (text_1[i+k] == text_2[j]):
-                    word += text_2[j]
-                    k += 1
-                elif k != 0: possible_result.append([word, k]); k = 0
-                j += 1
-            i += 1
-        result = possible_result[np.argmax(np.array(possible_result)[:,1])]
-        print(f"\nLongest substring is \"{result[0]}\" with \"{result[1]}\" letters.\n")
-        return result
+        possible_result, i = [], 0      # สร้างตัวแปร possible_result มาเพื่อเก็บค่าของ [word -> คำที่ซ้ำ, k -> จำนวนอักษร], i -> iteration couter.
+        while i < len(self.text_1):     # while ใน self.text_1, iteration couter -> i.
+            j, k, word = 0, 0, ""       # j -> iteration couter, k -> letter couter, word -> letter collecter.
+            while j < len(self.text_2): # while ใน self.text_2, iteration couter -> j.
+                if (text_1[i+k] == text_2[j]):      # เช็คว่าเจอตัวซ้ำมั้ย โดยถ้าเจอจะขยับขึ้นไปทีละ k หน่วย เพื่อไปยังตัวอักษรถัดไป.
+                    word += text_2[j]               # เก็บตัวอักษรที่เจอไว้ใน word.
+                    k += 1                          # เพิ่มค่าของ k ขึ้น 1 หน่วยเพื่อนับตัวซ้ำและใช้ในการขยับ.
+                elif k != 0: possible_result.append([word, k]); k = 0   # กรณีที่ผ่าน if แล้ว k != 0 จะทำการเก็บค่าของ [word, k] ไว้ใน possible_result.
+                j += 1  # increasing condition for loop j.
+            i += 1      # increasing condition for loop i.
+        result = possible_result[np.argmax(np.array(possible_result)[:,1])]                 # เก็บค่าผลลัพธ์ที่มากที่สุดไว้ในตัวแปร result.
+        print(f"\nLongest substring is \"{result[0]}\" with \"{result[1]}\" letters.\n")    # ปริ้นท์ต่านั้นออกมา.
+        return result   # return ค่านั้นออกไปเป็น list.
 
 if __name__ == "__main__":
     text_1 = "parachute"
