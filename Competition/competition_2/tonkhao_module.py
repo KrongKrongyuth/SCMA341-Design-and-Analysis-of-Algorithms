b3c9cfe91195cc9d4e7ec1a6f317013746b69688
TK <159685178+khaojao3360@users.noreply.github.com>
2024-03-29 21:18:52 +0700

    Create tonkhao_module.py
    this commit is for testing

---
+++ Competition/competition_2/tonkhao_module.py
@@ -0,0 +1,21 @@
+"""
+Compettion 2: Connected component algorithm.
+
+Group member
+    1. Krong        Krongyuth           6405009 (Submitter)
+    2. Nattawat     Trisatheanpisan     6405025
+    3. Shothibutr   Tansiri             6405203
+    4. Phumioat     kamthong            6405310
+
+Instructions
+    •Find connected component in an image.
+    •Given an array of integers (any size), find all connected components
+    (pixels with the same value as the adjacent pixel).
+    •Return list of arrays that separate each component apart in its own array.
+    •Make it runs as fast as possible.
+
+    •Submit in MS teams before 23.59 of April 2nd.
+    •Speed test in class of April 4th
+
+Module owner: Tonkhao
+"""
