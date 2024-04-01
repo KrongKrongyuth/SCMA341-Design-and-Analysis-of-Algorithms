906ca9c9bd5db8a8c3892357227fd4c6ded5b692
TON_KKrongyuth <ton.kkrongyuth@gmail.com>
2024-04-01 23:46:37 +0700

    ton commit -m runtime file

--- Competition/competition_2/competition_2_testcase.py
+++ Competition/competition_2/competition_2_testcase.py
@@ -4,7 +4,10 @@ Compettion 2: Connected component algorithm.
 """
 
 import matplotlib.pyplot as plt
-from ton_module import *
+import seaborn as sns
+import ton_module as ton
+import tao_module as tao
+import tonkhao_module as khao
 from time import process_time
 
 TEST_CASE = {"case_1": [[[1, 2, 2, 2, 4],
@@ -38,11 +41,12 @@ TEST_CASE = {"case_1": [[[1, 2, 2, 2, 4],
                         [None,  None, None,   1,    1 ]]]}
 
 if __name__ == "__main__":
-    cc = ConnectedComponent(TEST_CASE["case_1"][0])
+    input_grid = TEST_CASE["case_1"][0]
+    cc = ton.ConnectedComponent(input_grid)
     
     # Run time test
-    num_round = 10
-    ton_runtime, tonkhao_runtime, tao_runtime, kanthong_runtime, = [], [], [], []
+    num_round = 50
+    ton_runtime, tonkhao_runtime, tao_runtime, kanthong_runtime = [], [], [], []
     for _ in range(num_round):
         start = process_time()
         cc.find_connected(start_index = [0,0])
@@ -50,19 +54,34 @@ if __name__ == "__main__":
         ton_runtime.append(end - start)
         
         start = process_time()
-        cc.find_connected(start_index = [0,0])
+        tao.find_connected(input_grid)
         end = process_time()
         tao_runtime.append(end - start)
         
         start = process_time()
-        cc.find_connected(start_index = [0,0])
+        khao.find_connected(input_grid)
         end = process_time()
         tonkhao_runtime.append(end - start)
         
-        start = process_time()
-        cc.find_connected(start_index = [0,0])
-        end = process_time()
-        kanthong_runtime.append(end - start)
+        # start = process_time()
+        # cc.find_connected(start_index = [0,0])
+        # end = process_time()
+        # kanthong_runtime.append(end - start)
+
+    fig, axs = plt.subplots(2,3)
+    fig.suptitle('Algorithm runtime')
+    axs[0, 0].plot(range(num_round), ton_runtime)
+    axs[0, 0].set_title("Ton Runtime")
+    axs[0, 1].plot(range(num_round), tao_runtime)
+    axs[0, 1].set_title("Tao Runtime")
+    axs[0, 2].plot(range(num_round), tonkhao_runtime)
+    axs[0, 2].set_title("Tonkhao Runtime")
+    
+    sns.histplot(data = ton_runtime, ax = axs[1,0], kde = True)
+    sns.histplot(data = tao_runtime, ax = axs[1,1], kde = True)
+    sns.histplot(data = tonkhao_runtime, ax = axs[1,2], kde = True)
+    plt.tight_layout()
+    plt.show()
     
     # Test result
     # print(cc.find_connected(start_index = [1,1])) # PASS
--- Competition/competition_2/tao_module.py
+++ Competition/competition_2/tao_module.py
@@ -44,17 +44,17 @@ def find_connected(grid):
         print_grid(component_grid)
         print()
 
-grid2 = [
-    [1, 2, 2, 2, 1],
-    [1, 3, 3, 2, 1],
-    [1, 1, 3, 2, 1],
-    [3, 3, 3, 2, 1],
-    [3, 3, 3, 1, 1]
-]
+# grid2 = [
+#     [1, 2, 2, 2, 1],
+#     [1, 3, 3, 2, 1],
+#     [1, 1, 3, 2, 1],
+#     [3, 3, 3, 2, 1],
+#     [3, 3, 3, 1, 1]
+# ]
 
 
 
 
 
-find_connected(grid2)
+# find_connected(grid2)
 
